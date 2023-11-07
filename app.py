94a89e35a0e6442f74b1d28458ad8027066672e0
Phil_Puk <1phil.pukropski@gmx.de>
2023-11-07 21:52:10 +0100

    Added first plotting for tracked processes.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from logic.processes.tracked_processes import TrackedProcesses
 from utility.logger_config import log
 import time
 import psutil
+from matplotlib import pyplot as plt
+import numpy as np
 
 
 class App:
@@ -63,3 +65,33 @@ class App:
             log.info(f"Program stopped - running time: {time.time() - self._start}")
             self.tracked_processes.save()
             exit(0)
+
+
+tracked_processes = TrackedProcesses()
+
+times = []
+
+for process in tracked_processes.get_processes():
+    times.append(process.get_time()/60.)
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x=range(len(times)), height=times)
+
+ax.set_ylim(0, max(times))
+ax.set_ylabel('time in min')
+ax.set_xticks(np.add(range(len(times)), 0.5))
+ax.set_xticklabels([process.get_ui_name() for process in tracked_processes.get_processes()])
+
+
+def autolabel(rects):
+    # attach some text labels
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
+                '%d' % int(height),
+                ha='center', va='bottom')
+
+
+autolabel(rects1)
+
+plt.show()
